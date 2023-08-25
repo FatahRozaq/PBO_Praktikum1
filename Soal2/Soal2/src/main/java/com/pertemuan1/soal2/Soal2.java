0317f321e8626dc8a03f4784c955ac4f20dcdb62
FatahRozaq <fatah.rozaq20@gmail.com>
2023-08-25 22:43:20 +0700

    Folder ini berisi soal analisis 2

---
+++ Soal2/Soal2/src/main/java/com/pertemuan1/soal2/Soal2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pertemuan1.soal2;
+
+/**
+ *
+ * @author USER
+ */
+public class Soal2 {
+
+    public static void main(String[] args) {
+        int i = 42;
+        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        System.out.println(s);
+    }
+}
