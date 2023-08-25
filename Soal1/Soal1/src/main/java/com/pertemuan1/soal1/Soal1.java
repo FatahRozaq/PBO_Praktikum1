92cec57ed5c7fd5ea12a740b45feea457ddf2b71
FatahRozaq <fatah.rozaq20@gmail.com>
2023-08-25 22:42:34 +0700

    Folder ini berisi soal analisis 1

---
+++ Soal1/Soal1/src/main/java/com/pertemuan1/soal1/Soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pertemuan1.soal1;
+
+/**
+ *
+ * @author USER
+ */
+public class Soal1 {
+
+    public static void main(String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        int hasil = angka1 + angka2;
+        
+        System.out.println("Hasil 1: " + hasil);
+    }
+}
