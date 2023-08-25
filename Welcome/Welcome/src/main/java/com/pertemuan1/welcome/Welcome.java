69aa13a4418632a2dedcbdb7439405091010358a
FatahRozaq <fatah.rozaq20@gmail.com>
2023-08-25 22:43:55 +0700

    Folder ini berisi penyelesaian soal no 3

---
+++ Welcome/Welcome/src/main/java/com/pertemuan1/welcome/Welcome.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pertemuan1.welcome;
+
+/**
+ *
+ * @author USER
+ */
+//public class Welcome 
+//{
+//	public static void main(String[] args)
+//	{
+//		String greeting = "Welcome to Core Java!";
+//		System.out.println(greeting);
+//		for(int i = 0; i <greeting.length(); i++)
+//		{
+//			System.out.print("=");
+//			System.out.println();
+//		}
+//	}
+//}
+
+public class Soal1 {
+    public static void main(String[] args){
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1+angka2);
+
+        System.out.println("Hasil 1" +hasil);
+    }
+}
